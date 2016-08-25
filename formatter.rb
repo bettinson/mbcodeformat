6bd29683e488abaac1d142a22395e6ae147732e5
bettinson <mattbettinson@me.com>
2016-08-25 12:48:09 -0400

    Now indenting

--- formatter.rb
+++ formatter.rb
@@ -9,24 +9,28 @@ class Formatter
     @indent_level = 0
   end
 
-  def format(file)
+  def self.format(file)
     @file = file
-    @file.each { |line| format_line(line) }
+    #@file.each_line do |line| 
+    #  puts format_line(line) 
+    #end
+    @indent_level = 0
+    File.readlines(@file).each do |line|
+      self.format_line(line)
+    end
   end
 
-  def format_line(line)
+  private
+  def self.format_line(line)
     # Easier when line doesen't have any tabs, spaces etc. 
     # This is a very strict formatter
     line = line.strip
     
     #Make the line have the proper amount of indentation (in tabs, sorry Erik)
-    (i .. @indent_level).each do |i|
+    (0..@indent_level - 1).each do |i|
       line.prepend("\t")
     end
 
-    # TODO
-    # Still haven't decided whether I will change the file in-line or create a copy of it
-    # RIP Ram with big files if I copy it
     line.each_char { |c|
       case c
       when "{"
@@ -35,16 +39,16 @@ class Formatter
       when "}"
         @indent_level -= 1 unless @indent_level == 0 
         #Untab
+        line = line[1..-1]
       else
         #Character is a normal character
       end
     }
+    return line
   end
 end
-
-formatter = Formatter.new
 
 first_arg = ARGV[0]
 file = File.new(first_arg)
 
-puts formatter.format(file)
+Formatter.format(file)
